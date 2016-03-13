326410f1ad5b626eb62f8d45971ab62ff6ce1c67
Anders Weijnitz <anders.weijnitz@gmail.com>
2016-03-13 18:22:11 +0100

    Added hexdump

---
+++ hexdump.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var argv = require('minimist')(process.argv.slice(2));
+
+var width = argv.w || 16;
+
+var data = fs.readFileSync(argv._[0]);
+var nrWholeRows = Math.floor(data.length / width);
+
+// Do all complete rows
+for(var i = 0; i < nrWholeRows; i++) {
+  for (var w = 0; w < width; w++) {
+    var val = data.readUInt8(i).toString(16);
+
+    // Leading zero
+    if(val.length < 2)
+      console._stdout.write('0');
+    console._stdout.write(data.readUInt8(i).toString(16));
+
+    if(w < (width - 1))
+      console._stdout.write(',');
+  }
+  console._stdout.write('\n');
+
+}
+
+// Do remainder
+for(var r = 0; r < (data.length % width); r++) {
+  console._stdout.write(data.readUInt8(r).toString(16));
+}
+
+
---
+++ testHexdump.sh
@@ -0,0 +1,1 @@
+node hexdump.js -w 16  ./test/testdata/320x200px_LE_2200.bin
